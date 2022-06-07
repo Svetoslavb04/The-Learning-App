15383fccac217acf65ad5088b19c21ca4cd6b3eb
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-06-07 18:59:11 +0300

    Added Footer Component

--- the-learning-app/src/App.vue
+++ the-learning-app/src/App.vue
@@ -1,5 +1,8 @@
 <script setup>
+
 import { RouterLink, RouterView } from "vue-router";
+import Footer from "./components/Footer.vue"
+
 </script>
 
 <template>
@@ -7,14 +10,12 @@ import { RouterLink, RouterView } from "vue-router";
     <div class="router-content">
       <RouterView />
     </div>
-    <footer>
-      <RouterLink to="/" class="footer-navigation-link">Home</RouterLink>
-      <RouterLink to="/learning" class="footer-navigation-link">Learning</RouterLink>
-    </footer>
+    <Footer />
   </div>
 </template>
 
 <style>
+
 @import "@/assets/base.css";
 
 .content-wrapper {
@@ -27,18 +28,4 @@ import { RouterLink, RouterView } from "vue-router";
   flex-grow: 1;
 }
 
-footer {
-  height: 100px;
-  padding: 0px 40px;
-  background: #1b262c;
-  display: flex;
-  align-items: center;
-  gap: 20px
-}
-
-.footer-navigation-link {
-  font-size: 20px;
-  color: #BBE1FA;
-  text-decoration: none;
-}
 </style>
---
+++ the-learning-app/src/components/Footer.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <footer>
+    <RouterLink to="/" class="footer-navigation-link">Home</RouterLink>
+    <RouterLink to="/learning" class="footer-navigation-link">Learning</RouterLink>
+    <h2 class="footer-heading-text">The Learning App</h2>
+  </footer>
+</template>
+
+<style>
+footer {
+  height: 80px;
+  padding: 0px 40px;
+  font-size: 20px;
+  background: #1b262c;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+}
+
+.footer-navigation-link {
+  color: #bbe1fa;
+  text-decoration: none;
+}
+
+.footer-navigation-link:nth-child(2) {
+  flex-grow: 1;
+}
+
+.footer-heading-text {
+  color: #3282b8;
+}
+
+@media (max-width: 1000px) {
+  footer {
+    height: 75px;
+    padding: 0px 30px;
+    font-size: 18px;
+  }
+
+  .footer-heading-text {
+    font-size: 28px;
+  }
+}
+
+@media (max-width: 500px) {
+  footer {
+    height: 125px;
+    padding: 10px;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  .footer-heading-text {
+    font-size: 24px;
+    margin-bottom: 15px;
+  }
+}
+</style>
