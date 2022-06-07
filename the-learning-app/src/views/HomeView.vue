239a78f643785e49e5997abf00ea2fdfc34c5989
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-06-07 19:16:39 +0300

    Added Home page heading

--- the-learning-app/src/views/HomeView.vue
+++ the-learning-app/src/views/HomeView.vue
@@ -1,7 +1,37 @@
 <script setup></script>
 
 <template>
-  <main>
-    <div></div>
+  <main class="main-content">
+    <div class="home-page-heading-wrapper">
+      <h1 class="home-page-heading">Learning</h1>
+    </div>
+    <div class="home-page-sub-heading-wrapper">
+      <h2 class="home-page-sub-heading">
+        You want to track your learning process. Keep all of your courses in one place.
+        Here you can add your courses, edit them and delete them.
+      </h2>
+    </div>
   </main>
 </template>
+
+<style>
+.main-content {
+  text-align: center;
+}
+
+.home-page-heading-wrapper {
+  margin: 30px auto 20px auto;
+}
+
+.home-page-sub-heading-wrapper {
+  margin: 20px auto;
+  width: 35%;
+}
+
+@media (max-width: 1000px) {
+  .home-page-sub-heading-wrapper {
+    margin: 20px auto;
+    width: 50%;
+  }
+}
+</style>
